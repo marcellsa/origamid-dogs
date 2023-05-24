f41c6671d710dda6fb7b7816f1e1a04d6601b13b
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-05-24 17:04:39 -0300

    feat: useForms.jsx

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useForm from '../Hooks/useForms';
 import FormsButton from './FormsButton';
 import FormsInput from './FormsInput';
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const username = useForm();
+  const password = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({username, password})
-    }).then((response) => {
-      console.log(response);
-      return response.json();
-    }).then((json) => {
-      console.log(json);
-    });
+
+    if (username.validate() && password.validate()){
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(),
+      })
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
   }
 
   return (
